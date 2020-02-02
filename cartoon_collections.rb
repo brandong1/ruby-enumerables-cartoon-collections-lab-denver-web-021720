52d6101bb55591e767cab83334bfa65a47accf4d
Brandon Green <brandon@brandonleeproductions.com>
2020-02-02 17:04:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,10 @@ def long_planeteer_calls(planet_calls)
   planet_calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
