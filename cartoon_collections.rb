7fdb1c69d5256b04b013290ebf32abdfcbc433ca
Brandon Green <brandon@brandonleeproductions.com>
2020-02-02 17:02:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,6 @@ def long_planeteer_calls(planet_calls)
   planet_calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
