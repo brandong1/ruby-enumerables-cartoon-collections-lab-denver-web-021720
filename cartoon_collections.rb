435f14ab1526ff8203428d83709f9f992bc188bd
Brandon Green <brandon@brandonleeproductions.com>
2020-02-02 17:02:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ def summon_captain_planet(planet_calls)
   planet_calls.map { |call| call.capitalize + '!' }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planet_calls)
+  planet_calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
