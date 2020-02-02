40d9f0466e2883fad10043dfe791766755c0cfdc
Brandon Green <brandon@brandonleeproductions.com>
2020-02-02 16:51:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarf_array)
-  dwarf_array.each_with_index{|d, i| p dwarf_array}
+  dwarf_array.each_with_index{|d, i| }
 end
 
 def summon_captain_planet# code an argument here
