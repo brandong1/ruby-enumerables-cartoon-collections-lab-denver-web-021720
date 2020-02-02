9cf4da35a0f5a9fa6c9311dbfdc4d76cd02d1ff1
Brandon Green <brandon@brandonleeproductions.com>
2020-02-02 17:07:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,9 @@ def long_planeteer_calls(planet_calls)
   planet_calls.any? {|call| call.length > 4}
 end
 
+# Was I supposed to know to manually create this array?":
+potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
+
 def find_the_cheese(potentially_cheesy_items)
   cheeses = %w[gouda cheddar camembert]
 
